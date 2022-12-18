fe2d4f7b1d9f7440eca53cff0ad7ff36a8e6f503
aq032 <anthonyq032@gmail.com>
2022-12-17 19:11:11 -0700

    Add testing code for MD Editor on main page

--- src/App.js
+++ src/App.js
@@ -2,29 +2,54 @@ import './App.css';
 import { useRef, useState } from 'react';
 import NewPage from './components/newPage';
 
+
+import MDEditor from '@uiw/react-md-editor';
+
+// Markdown to jsx
+// const App = () => {
+//   const userText = useRef("");
+
+//   const [textData, setTextData] = useState("## Your page will generate here!");
+
+//   // Sets textData for markup
+//   const handleClick = event => {
+//     // setTextData(JSON.stringify(userText.current.value));
+//     setTextData(userText.current.value);
+//   };
+
+//   return (
+//     <main>
+//       <label htmlFor="user-text-area">User form</label>
+//       <textarea placeholder="Input your markdown here to generate a new page!" ref={userText} id="user-text-area" name="user-text-area" rows="30" cols="75" />
+
+//       <button onClick={handleClick}>Generate Page</button>
+
+//       {/* <NewPage data={typeof (textData) === Object ? JSON.parse(textData) : textData} /> */}
+//       <NewPage data={textData} />
+//     </main>
+//   );
+// };
+
+
+// MD Editor
 const App = () => {
-  const userText = useRef("");
-
-  const [textData, setTextData] = useState("## Your page will generate here!");
-
-  // Sets textData for markup
-  const handleClick = event => {
-    // setTextData(JSON.stringify(userText.current.value));
-    setTextData(userText.current.value);
-  };
+  const [value, setValue] = useState("**Hello world!!!**");
 
   return (
-    <main>
-      <label htmlFor="user-text-area">User form</label>
-      <textarea placeholder="Input your markdown here to generate a new page!" ref={userText} id="user-text-area" name="user-text-area" rows="30" cols="75" />
+    <div className="container">
+      <MDEditor
+        value={value}
+        onChange={setValue}
+      />
 
-      <button onClick={handleClick}>Generate Page</button>
+      <h1>Your generated page!</h1>
+      <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} />
 
-      {/* <NewPage data={typeof (textData) === Object ? JSON.parse(textData) : textData} /> */}
-      <NewPage data={textData} />
-    </main>
+      {/* Can be safely and easily converted back and forth from string to JSON object without any issues */}
+      <MDEditor.Markdown source={JSON.parse(JSON.stringify(value))} style={{ whiteSpace: 'pre-wrap' }} />
+    </div>
   );
-};
+}
 
 
 export default App;
